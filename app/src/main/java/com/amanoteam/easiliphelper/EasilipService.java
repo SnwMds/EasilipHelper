d013cc6839eeff2e1c535d43169cb9c341932227
SnwMds <46784020+SnwMds@users.noreply.github.com>
2021-11-03 09:48:31 -0300

    Initial commit

---
+++ app/src/main/java/com/amanoteam/easiliphelper/EasilipService.java
@@ -0,0 +1,107 @@
+package com.amanoteam.easiliphelper;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.os.Parcelable;
+import android.os.Process;
+
+public class EasilipService extends Service {
+	
+	private Looper serviceLooper;
+	private ServiceHandler serviceHandler;
+	
+	private PackageManager packageManager;
+	
+	// Handler that receives messages from the thread
+	private final class ServiceHandler extends Handler {
+			public ServiceHandler(final Looper looper) {
+					super(looper);
+			}
+			@Override
+			public void handleMessage(final Message msg) {
+					
+					final Intent intent = (Intent) msg.obj;
+					final String action = intent.getStringExtra("action");
+					
+					if action.equals("fetch_installed_extensions") {
+						final List<ApplicationInfo> packages = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
+						
+						String packageLabel;
+						String packageName;
+						int versionCode;
+						String versionName;
+						
+						JSONObject jsonObject;
+						
+						PackageInfo packageInfo;
+						
+						final JSONArray jsonArray = new JSONArray();
+						
+						for (ApplicationInfo applicationInfo : packages) {
+							packageLabel = packageManager.getApplicationLabel(applicationInfo);
+							packageName = applicationInfo.packageName;
+							
+							packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+							
+							versionCode = packageInfo.versionCode;
+							versionName = packageInfo.versionName;
+							
+							jsonObject = new JSONObject():
+							
+							jsonObject.put("packageLabel", packageLabel);
+							jsonObject.put("packageName", packageName);
+							jsonObject.put("versionCode", versionCode);
+							jsonObject.put("versionName", versionName);
+							
+							jsonArray.put(jsonObject);
+						}
+						
+					}
+					
+					stopSelf(msg.arg1);
+			}
+	}
+
+	@Override
+	public void onCreate() {
+		// Set package manager
+		packageManager = getPackageManager();
+		
+		final HandlerThread thread = new HandlerThread("ServiceStartArguments",
+						Process.THREAD_PRIORITY_BACKGROUND);
+		thread.start();
+
+		serviceLooper = thread.getLooper();
+		serviceHandler = new ServiceHandler(serviceLooper);
+	}
+
+	@Override
+	public int onStartCommand(final Intent intent, final int flags, final int startId) {
+			Message msg = serviceHandler.obtainMessage();
+			msg.arg1 = startId;
+			msg.obj = (Object) intent;
+			serviceHandler.sendMessage(msg);
+			
+			return START_NOT_STICKY;
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+			return null;
+	}
+
+	@Override
+	public void onDestroy() {
+		System.exit(0);
+	}
+
+
+}
---
+++ app/src/main/java/com/amanoteam/easiliphelper/MainActivity.java
@@ -0,0 +1,44 @@
+package com.amanoteam.easiliphelper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.amanoteam.easiliphelper.EasilipService;
+
+public class MainActivity extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		
+		if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+			requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+		}
+		
+		final Intent intent = getIntent();
+		final String action = intent.getStringExtra("action");
+		
+		final Intent newIntent = new Intent(this, EasilipService.class);
+		
+		newIntent.putExtra("action", action);
+		
+		startService(newIntent);
+		
+		finish();
+	}
+	
+	@Override
+	public void onRequestPermissionsResult(final int requestCode, final String[] permissions, final int[] grantResults) {
+		
+		if (grantResults.length == 0 || grantResults.get(0) != PackageManager.PERMISSION_GRANTED) {
+			Toast.makeText(getApplicationContext(), "Storage permission is required for this extension to work properly!", Toast.LENGTH_SHORT).show();
+		}
+		
+	}
+
+}
