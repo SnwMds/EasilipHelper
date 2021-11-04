671aa16ee5db35893b8ca94ecf656d1a67895c10
SnwMds <46784020+SnwMds@users.noreply.github.com>
2021-11-04 00:06:07 -0300

    Fix build

--- app/src/main/java/com/amanoteam/easiliphelper/GetPackagesService.java
+++ app/src/main/java/com/amanoteam/easiliphelper/GetPackagesService.java
@@ -30,11 +30,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 public class GetPackagesService extends Service {
+	
 	private Looper serviceLooper;
 	private ServiceHandler serviceHandler;
-
-
+	
 	private PackageManager packageManager;
+	
 	// Handler that receives messages from the thread
 	private final class ServiceHandler extends Handler {
 		public ServiceHandler(Looper looper) {
@@ -42,14 +43,12 @@ public class GetPackagesService extends Service {
 		}
 		@Override
 		public void handleMessage(Message msg) {
-			// Normally we would do some work here, like download a file.
-			// For our sample, we just sleep for 5 seconds.
 			
 			final List<ApplicationInfo> packages = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
 			
 			String packageLabel;
 			String packageName;
-			int versionCode;
+			long versionCode;
 			String versionName;
 			
 			PackageInfo packageInfo;
@@ -59,12 +58,16 @@ public class GetPackagesService extends Service {
 			
 			try {
 				for (ApplicationInfo applicationInfo : packages) {
-					packageLabel = packageManager.getApplicationLabel(applicationInfo).toString();
 					packageName = applicationInfo.packageName;
+					
+					if (!packageName.startsWith("eu.kanade.tachiyomi.extension")) {
+						continue;
+					}
 					
 					packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
 					
-					versionCode = packageInfo.versionCode;
+					packageLabel = packageManager.getApplicationLabel(applicationInfo).toString();
+					versionCode = packageInfo.getLongVersionCode();
 					versionName = packageInfo.versionName;
 					
 					jsonObject = new JSONObject();
@@ -88,50 +91,39 @@ public class GetPackagesService extends Service {
 				Log.d("myapp", Log.getStackTraceString(e));
 			}
 			
-			// Stop the service using the startId, so that we don't stop
-			// the service in the middle of handling another job
 			stopSelf(msg.arg1);
 		}
 	}
 
 	@Override
 	public void onCreate() {
-	packageManager = getPackageManager();
-	// Start up the thread running the service. Note that we create a
-	// separate thread because the service normally runs in the process's
-	// main thread, which we don't want to block. We also make it
-	// background priority so CPU-intensive work doesn't disrupt our UI.
-	HandlerThread thread = new HandlerThread("ServiceStartArguments",
-			Process.THREAD_PRIORITY_BACKGROUND);
-	thread.start();
-
-	// Get the HandlerThread's Looper and use it for our Handler
-	serviceLooper = thread.getLooper();
-	serviceHandler = new ServiceHandler(serviceLooper);
+		packageManager = getPackageManager();
+		
+		HandlerThread thread = new HandlerThread("ServiceStartArguments", Process.THREAD_PRIORITY_BACKGROUND);
+		thread.start();
+		
+		serviceLooper = thread.getLooper();
+		serviceHandler = new ServiceHandler(serviceLooper);
 	}
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
-
-		// For each start request, send a message to start a job and deliver the
-		// start ID so we know which request we're stopping when we finish the job
+		
 		Message msg = serviceHandler.obtainMessage();
 		msg.arg1 = startId;
 		serviceHandler.sendMessage(msg);
-
-		// If we get killed, after returning from here, restart
+		
 		return START_STICKY;
 	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
-		// We don't provide binding, so return null
 		return null;
 	}
 
 	@Override
 	public void onDestroy() {
-	Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show();
+		Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show();
 	}
 }
