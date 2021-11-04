30e8b17460c483c683fb42676228874470430fb2
SnwMds <46784020+SnwMds@users.noreply.github.com>
2021-11-04 15:54:45 -0300

    Fix build

--- app/src/main/java/com/amanoteam/easiliphelper/activities/SetupPermissionsActivity.java
+++ app/src/main/java/com/amanoteam/easiliphelper/activities/SetupPermissionsActivity.java
@@ -2,6 +2,7 @@ package com.amanoteam.easiliphelper.activities;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -20,7 +21,7 @@ public class SetupPermissionsActivity extends Activity {
 		
 		Toast.makeText(this, "Please grant storage permission", Toast.LENGTH_SHORT).show();
 		
-		requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+		requestPermissions(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
 		
 		finish();
 	}
